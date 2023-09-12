3dd5c1a9f54dce509178e2c283ed21e5e850b6f7
Sawyer90 <144781584+Sawyer90@users.noreply.github.com>
2023-09-12 06:35:18 -0300

    Add files via upload
    Files uploaded 12/9

---
+++ java.js
@@ -0,0 +1,14 @@
+document.getElementById("boton").onclick = function () {
+    
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+}
+
+document.getElementById("menu1").onclick = function () {
+
+  window.scrollTo(0,position(document.getElementById("jtts1")))
+   
+  
+}
